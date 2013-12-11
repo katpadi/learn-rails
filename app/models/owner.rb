35437b951a0ee9c42aadbd1d3e474296fa6c6037
Kat Padilla <katp@sakurasky.com>
2013-12-11 17:38:35 +0800

    dynamic homepage

---
+++ app/models/owner.rb
@@ -0,0 +1,21 @@
+class Owner
+
+    def name
+        name = 'Kat'
+    end
+
+    def birthdate
+        birthdate = Date.new(1988, 9, 20)
+    end
+
+    def countdown
+        today = Date.today
+        birthday = Date.new(today.year, birthdate.month, birthdate.day)
+        if birthday > today
+            countdown = (birthday - today).to_i
+        else
+            countdown = (birthday.next_year - today).to_i
+        end
+    end
+
+end
